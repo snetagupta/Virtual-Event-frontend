79d695adf37d199519caec4cd1db3359eee4fd4c
sneta gupta <snetagupta4nov@gmail.com>
2024-08-17 13:57:44 +0530

    Add navbar and carousel page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Carousel from "../components/Carousel";
+import Navbar from "../components/Navbar";
+
+const Home = () =>{
+    return(
+        <div>
+<Navbar/>
+<Carousel/>
+        </div>
+    )
+}
+export default Home;
