b22837fff4df403cbc1002533be6d28319ecd825
sneta gupta <snetagupta4nov@gmail.com>
2024-09-09 21:58:41 +0530

    fix bugs

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -293,14 +293,4 @@ const Events = () => {
   );
 };
 
-// import EventSection from "../components/events/EventSection";
-
-// const Events = () => {
-//   return (
-//     <div>
-//       <EventSection />
-//     </div>
-//   );
-// };
-
 export default Events;
